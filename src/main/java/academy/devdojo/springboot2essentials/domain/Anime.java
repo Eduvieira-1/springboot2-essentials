899d1ba9a38c203bcb459a29130d3d5d505a497d
Eduvieira-1 <edu.vieira1@icloud.com>
2023-09-27 16:36:51 -0300

    connect mysql

--- src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -1,21 +1,16 @@
 package academy.devdojo.springboot2essentials.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
 public class Anime {
+
+    private Long id;
     private String name;
-
-    public Anime(String name) {
-        this.name = name;
-    }
-
-    public Anime(){
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
